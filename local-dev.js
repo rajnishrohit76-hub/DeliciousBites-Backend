76b8c6df9ffd1d0fa4a959d87a947681b56dbfe4
unknown <rajnishrohit76@gmail.com.com>
2025-12-06 10:19:52 +0530

    Updated project files

--- local-dev.js
+++ local-dev.js
@@ -1,4 +1,2 @@
-const app = require("./Server");
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log(`Local server running on ${PORT}`));
+const app = require("./server");
+app.listen(3000, () => console.log("Running locally on http://localhost:3000"));
